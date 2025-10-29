4d40123b20ae605000f20264c869ce059e8290dd
Devansh Tiwari <devanshtiwari817@gmail.com>
2025-10-29 23:33:47 +0530

    Update CSS

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,114 +1,247 @@
-import { Plus, Trash2 } from 'lucide-react';
-import React from 'react'
+// import { Plus, Trash2 } from 'lucide-react';
+// import React from 'react'
+
+// const ProjectForm = ({ data, onChange }) => {
+
+//     const addProject = () => {
+//         const newProject = {
+//             institution: "",
+//             name: "",
+//             type: "",
+//             description: "",
+//             start_date: "",
+//             end_date: "",
+//             link: "",
+//         };
+//         onChange([...data, newProject])
+//     }
+
+//     const removeProject = (index) => {
+//         const updated = data.filter((_, i) => i != index);
+//         onChange(updated)
+//     }
+
+//     const updateProject = (index, field, value) => {
+//         const updated = [...data];
+//         updated[index] = { ...updated[index], [field]: value }
+//         onChange(updated)
+//     }
+
+//     return (
+//         // <div>
+//          <div className="max-w-full p-4 sm:p-6 bg-white rounded-md shadow-sm">
+//             <div className='flex items-center justify-between'>
+//                 <div>
+//                     <h3 className='flex items-center gap-2 text-lg font-semibold
+//                     text-gray-900'> Projects </h3>
+//                     <p className='text-sm text-gray-500'>Add your projects details</p>
+//                 </div>
+//                 <button onClick={addProject} className='flex items-center gap-2 px-3 py-1 text-sm bg-green-100
+//                 text-green-700 rounded-lg hover:bg-green-200 transition-colors
+//                 '>
+//                     <Plus className='size-4' />
+//                     Add Project
+//                 </button>
+//             </div>
+
+//             <div className='space-y-4 mt-6'>
+//                 {data.map((project, index) => (
+//                     <div key={index} className="p-4 border border-gray-200 rounded-lg space-y-3">
+//                         <div className='flex justify-between items-start'>
+//                             <h4>Project #{index + 1}</h4>
+//                             <button onClick={() => removeProject(index)}
+//                                 className='text-red-500 hover:text-red-700 transition-colors'>
+//                                 <Trash2 className="size-4" />
+//                             </button>
+//                         </div>
+//                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+//                             <input
+//                                 value={project.name || ""}
+//                                 onChange={(e) => updateProject(index, "name", e.target.value)}
+//                                 type="text"
+//                                 placeholder="Project Name"
+//                                 // className="px-3 py-2 text-sm rounded-lg"
+//                                  className="w-full px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none break-words"
+//                             />
+//                             <input
+//                                 value={project.type || ""}
+//                                 onChange={(e) => updateProject(index, "type", e.target.value)}
+//                                 type="text"
+//                                 placeholder="Project Type"
+//                                 // className="px-3 py-2 text-sm rounded-lg"
+//                                  className="w-full px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none break-words"
+//                             />
+//                             <input
+//                                 value={project.description || ""}
+//                                 onChange={(e) => updateProject(index, "description", e.target.value)}
+//                                 placeholder='Describe your Project...'
+//                                 className="px-3 py-2 text-sm rounded-lg resize-none w-full col-span-2"
+//                             />
+//                             <div className="flex gap-2 col-span-2">
+//                                 <input
+//                                     value={project.start_date || ""}
+//                                     onChange={(e) => updateProject(index, "start_date", e.target.value)}
+//                                     type="month"
+//                                     // className="px-3 py-2 text-sm w-1/2"
+//                                     className="w-full md:w-auto px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none"
+//                                 />
+//                                 <input
+//                                     value={project.end_date || ""}
+//                                     onChange={(e) => updateProject(index, "end_date", e.target.value)}
+//                                     type="month"
+//                                     // className="px-3 py-2 text-sm w-1/2"
+//                                     className="w-full md:w-auto px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none"
+//                                 />
+//                             </div>
+
+//                             <input
+//                                 value={project.link || ""}
+//                                 onChange={(e) => updateProject(index, "link", e.target.value)}
+//                                 type="url"
+//                                 placeholder="Link (optional)"
+//                                 className="px-3 py-2 text-sm rounded-lg"
+//                             />
+
+//                         </div>
+
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default ProjectForm
+
+
+
+
+
+import { Plus, Trash2 } from "lucide-react";
+import React from "react";
 
 const ProjectForm = ({ data, onChange }) => {
+  const addProject = () => {
+    const newProject = {
+      institution: "",
+      name: "",
+      type: "",
+      description: "",
+      start_date: "",
+      end_date: "",
+      link: "",
+    };
+    onChange([...data, newProject]);
+  };
 
-    const addProject = () => {
-        const newProject = {
-            institution: "",
-            name: "",
-            type: "",
-            description: "",
-            start_date: "",
-            end_date: "",
-            link: "",
-        };
-        onChange([...data, newProject])
-    }
+  const removeProject = (index) => {
+    const updated = data.filter((_, i) => i !== index);
+    onChange(updated);
+  };
 
-    const removeProject = (index) => {
-        const updated = data.filter((_, i) => i != index);
-        onChange(updated)
-    }
+  const updateProject = (index, field, value) => {
+    const updated = [...data];
+    updated[index] = { ...updated[index], [field]: value };
+    onChange(updated);
+  };
 
-    const updateProject = (index, field, value) => {
-        const updated = [...data];
-        updated[index] = { ...updated[index], [field]: value }
-        onChange(updated)
-    }
-
-    return (
-        // <div>
-         <div className="max-w-full p-4 sm:p-6 bg-white rounded-md shadow-sm">
-            <div className='flex items-center justify-between'>
-                <div>
-                    <h3 className='flex items-center gap-2 text-lg font-semibold
-                    text-gray-900'> Projects </h3>
-                    <p className='text-sm text-gray-500'>Add your projects details</p>
-                </div>
-                <button onClick={addProject} className='flex items-center gap-2 px-3 py-1 text-sm bg-green-100
-                text-green-700 rounded-lg hover:bg-green-200 transition-colors
-                '>
-                    <Plus className='size-4' />
-                    Add Project
-                </button>
-            </div>
-
-            <div className='space-y-4 mt-6'>
-                {data.map((project, index) => (
-                    <div key={index} className="p-4 border border-gray-200 rounded-lg space-y-3">
-                        <div className='flex justify-between items-start'>
-                            <h4>Project #{index + 1}</h4>
-                            <button onClick={() => removeProject(index)}
-                                className='text-red-500 hover:text-red-700 transition-colors'>
-                                <Trash2 className="size-4" />
-                            </button>
-                        </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                            <input
-                                value={project.name || ""}
-                                onChange={(e) => updateProject(index, "name", e.target.value)}
-                                type="text"
-                                placeholder="Project Name"
-                                // className="px-3 py-2 text-sm rounded-lg"
-                                 className="w-full px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none break-words"
-                            />
-                            <input
-                                value={project.type || ""}
-                                onChange={(e) => updateProject(index, "type", e.target.value)}
-                                type="text"
-                                placeholder="Project Type"
-                                // className="px-3 py-2 text-sm rounded-lg"
-                                 className="w-full px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none break-words"
-                            />
-                            <input
-                                value={project.description || ""}
-                                onChange={(e) => updateProject(index, "description", e.target.value)}
-                                placeholder='Describe your Project...'
-                                className="px-3 py-2 text-sm rounded-lg resize-none w-full col-span-2"
-                            />
-                            <div className="flex gap-2 col-span-2">
-                                <input
-                                    value={project.start_date || ""}
-                                    onChange={(e) => updateProject(index, "start_date", e.target.value)}
-                                    type="month"
-                                    // className="px-3 py-2 text-sm w-1/2"
-                                    className="w-full md:w-auto px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none"
-                                />
-                                <input
-                                    value={project.end_date || ""}
-                                    onChange={(e) => updateProject(index, "end_date", e.target.value)}
-                                    type="month"
-                                    // className="px-3 py-2 text-sm w-1/2"
-                                    className="w-full md:w-auto px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none"
-                                />
-                            </div>
-
-                            <input
-                                value={project.link || ""}
-                                onChange={(e) => updateProject(index, "link", e.target.value)}
-                                type="url"
-                                placeholder="Link (optional)"
-                                className="px-3 py-2 text-sm rounded-lg"
-                            />
-
-                        </div>
-
-                    </div>
-                ))}
-            </div>
+  return (
+    <div className="max-w-full p-4 sm:p-6 bg-white rounded-md shadow-sm">
+      {/* Header */}
+      <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-3">
+        <div>
+          <h3 className="flex items-center gap-2 text-lg font-semibold text-gray-900">
+            Projects
+          </h3>
+          <p className="text-sm text-gray-500">Add your project details</p>
         </div>
-    )
-}
+        <button
+          onClick={addProject}
+          className="flex items-center gap-2 px-4 py-2 text-sm bg-green-100
+          text-green-700 rounded-lg hover:bg-green-200 transition-colors w-full sm:w-auto justify-center"
+        >
+          <Plus className="size-4" />
+          Add Project
+        </button>
+      </div>
 
-export default ProjectForm
+      {/* Project Forms */}
+      <div className="space-y-5 mt-6">
+        {data.map((project, index) => (
+          <div
+            key={index}
+            className="p-4 border border-gray-200 rounded-lg space-y-4"
+          >
+            <div className="flex items-center justify-between">
+              <h4 className="font-medium text-gray-800">
+                Project #{index + 1}
+              </h4>
+              <button
+                onClick={() => removeProject(index)}
+                className="text-red-500 hover:text-red-700 transition-colors"
+              >
+                <Trash2 className="size-4" />
+              </button>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <input
+                value={project.name || ""}
+                onChange={(e) => updateProject(index, "name", e.target.value)}
+                type="text"
+                placeholder="Project Name"
+                className="w-full px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-200"
+              />
+              <input
+                value={project.type || ""}
+                onChange={(e) => updateProject(index, "type", e.target.value)}
+                type="text"
+                placeholder="Project Type"
+                className="w-full px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-200"
+              />
+
+              <textarea
+                value={project.description || ""}
+                onChange={(e) =>
+                  updateProject(index, "description", e.target.value)
+                }
+                placeholder="Describe your Project..."
+                className="col-span-1 sm:col-span-2 px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-200 resize-none"
+                rows="3"
+              />
+
+              <div className="flex flex-col sm:flex-row gap-4 col-span-1 sm:col-span-2">
+                <input
+                  value={project.start_date || ""}
+                  onChange={(e) =>
+                    updateProject(index, "start_date", e.target.value)
+                  }
+                  type="month"
+                  className="w-full px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-200"
+                />
+                <input
+                  value={project.end_date || ""}
+                  onChange={(e) =>
+                    updateProject(index, "end_date", e.target.value)
+                  }
+                  type="month"
+                  className="w-full px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-200"
+                />
+              </div>
+
+              <input
+                value={project.link || ""}
+                onChange={(e) => updateProject(index, "link", e.target.value)}
+                type="url"
+                placeholder="Project Link (optional)"
+                className="col-span-1 sm:col-span-2 w-full px-3 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-200"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectForm;
